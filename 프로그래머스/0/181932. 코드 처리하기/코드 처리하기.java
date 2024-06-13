3318f3a6b991760c90bd83f0f11839cc9f374abd
CINI <128473266+shinheekim@users.noreply.github.com>
2024-06-13 22:48:35 +0900

    [level 0] Title: 코드 처리하기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,28 +1,29 @@
 class Solution {
     public String solution(String code) {
-        StringBuilder ret = new StringBuilder();
+        String ret = "";
         int mode = 0;
-        
+
         for (int i = 0; i < code.length(); i++){
-            char ch = code.charAt(i);
             if (mode == 0){
-                if ((ch != '1') && (i % 2 == 0)) {
-                    ret.append(ch);
-                } else if (ch == '1'){
+                if ((code.charAt(i)!='1') && (i % 2 == 0)) {
+                    // ret.append(code.charAt(i));String에는 .append()불가
+                    ret += code.charAt(i);
+                } else if (code.charAt(i) == '1'){	
                     mode = 1;
                 }
             } else if (mode == 1){
-                if ((ch != '1') && (i % 2 != 0)){
-                    ret.append(ch);
-                } else if (ch == '1'){
+                if ((code.charAt(i) != '1') && (i % 2 != 0)){
+                    ret += code.charAt(i);
+                } else if (code.charAt(i) == '1'){
                     mode = 0;
                 }
             }
-        }
+          }
 
-        if (ret.length() == 0){
+        if (ret.equals("")){
             return "EMPTY";
-        }else{
-        return ret.toString();
+        } else {
+        return ret;
         }
-    }}
+    }
+}
