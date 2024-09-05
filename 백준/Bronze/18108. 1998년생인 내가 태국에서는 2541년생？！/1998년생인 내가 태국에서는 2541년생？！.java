3f8a41bd070533f392067ced9ccce4727a70944e
shinheekim <128473266+shinheekim@users.noreply.github.com>
2024-09-06 00:26:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 168 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        int diff = 543;
+        
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(y - diff);
+        
+    }
+}
