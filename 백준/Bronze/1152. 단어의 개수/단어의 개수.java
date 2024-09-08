581c6fc335959ff2ec31634625e17d7d54c3646d
shinheekim <128473266+shinheekim@users.noreply.github.com>
2024-09-08 15:47:01 +0900

    [Bronze II] Title: 단어의 개수, Time: 240 ms, Memory: 27864 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,12 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
         String[] strSplit = str.strip().split(" ");
 
         System.out.println(printLength(strSplit));
+        bf.close();
     }
 
     static long printLength(String[] strSplit){
