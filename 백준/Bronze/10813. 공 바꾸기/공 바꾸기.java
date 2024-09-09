1fe33247083676a959e20235e999597703e1c243
shinheekim <128473266+shinheekim@users.noreply.github.com>
2024-09-09 23:28:34 +0900

    [Bronze II] Title: 공 바꾸기, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -5,30 +5,33 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String firstReadLine = br.readLine();
-        String[] firstReadLineArray = firstReadLine.split(" ");
+        String[] str = br.readLine().split(" ");
 
-        int basketNum = Integer.parseInt(firstReadLineArray[0]);
-        int changeNum = Integer.parseInt(firstReadLineArray[1]);
+        int basketNum = Integer.parseInt(str[0]);
+        int changeNum = Integer.parseInt(str[1]);
 
-        Integer[] basket = new Integer[basketNum];
+        int [] basket = new int[basketNum];
 
         for (int i = 0; i < basketNum; i++) {
             basket[i] = i + 1;
         }
         for (int i = 0; i < changeNum; i++) {
-            String nextReadLine = br.readLine();
-            String[] nextReadLineArray = nextReadLine.split(" ");
+            String[] nextReadLineArray = br.readLine().split(" ");
 
             int ball1 = Integer.parseInt(nextReadLineArray[0]);
             int ball2 = Integer.parseInt(nextReadLineArray[1]);
 
+            // 교환
             int temp = basket[ball1 - 1];
             basket[ball1 - 1] = basket[ball2 -1];
             basket[ball2 - 1] = temp;
         }
+
+        StringBuilder sb = new StringBuilder();
         for (int printNum : basket) {
-            System.out.print(printNum + " ");
+            sb.append(printNum).append(" ");
         }
+        System.out.print(sb.toString());
+
     }
 }
