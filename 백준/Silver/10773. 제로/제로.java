4cdc1cf9789733f23198a1b75036534eb552d253
shinheekim <128473266+shinheekim@users.noreply.github.com>
2024-09-16 00:27:04 +0900

    [Silver IV] Title: 제로, Time: 200 ms, Memory: 23552 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,27 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
 
         for(int i = 0; i < num; i++) {
             int number = Integer.parseInt(br.readLine());
 
             if(number == 0) {
-                stack.pop();
+                ad.pop();
             }
             else {
-                stack.push(number);
+                ad.push(number);
             }
         }
-        
+
         int sum = 0;
-        for (int s : stack) {
+        for (int s : ad) {
             sum += s;
         }
         System.out.println(sum);
