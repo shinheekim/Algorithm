b3ba376f81673edd1759c90a47eeb7d9091e0609
shinheekim <128473266+shinheekim@users.noreply.github.com>
2024-09-16 00:28:08 +0900

    [Silver IV] Title: 제로, Time: 196 ms, Memory: 24124 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -2,12 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        Deque<Integer> ad = new ArrayDeque<>();
 
         for(int i = 0; i < num; i++) {
             int number = Integer.parseInt(br.readLine());
