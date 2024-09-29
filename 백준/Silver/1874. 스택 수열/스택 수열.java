72cbb543319a27b906c0aac8d659a135654f8ec7
shinheekim <128473266+shinheekim@users.noreply.github.com>
2024-09-29 16:18:57 +0900

    [Silver II] Title: 스택 수열, Time: 288 ms, Memory: 26376 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,10 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import java.util.ArrayDeque;
 import java.util.Stack;
 
 public class Main { // 스택 수열
-    final static Stack<Integer> STACK = new Stack<>();  // 사용자가 원하는 수열을 만들기 위한 stack
+    final static ArrayDeque<Integer> STACK = new ArrayDeque<>();  // 사용자가 원하는 수열을 만들기 위한 stack
     static StringBuilder sb = new StringBuilder();  // 출력
     static int[] INPUT; // 수열 입력 받기
 
